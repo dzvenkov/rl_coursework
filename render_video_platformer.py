e9ebbaaa498a23e34230fa30c2e7e5695ecfc2ff
dzvenkov <32355948+dzvenkov@users.noreply.github.com>
2025-04-13 16:35:45 +0100

    dqn 5 results

--- render_video_platformer.py
+++ render_video_platformer.py
@@ -22,7 +22,7 @@ def make_env():
     env.observation_space = gym.spaces.Box(low=0, high=255, shape=(4, 84, 84), dtype=np.uint8)
     return env
 
-experiment = 2
+experiment = "5a"
 
 env = PlatformerEnv(screen_w=800, screen_h=600, max_frames=1000)
 root_env = env
@@ -37,7 +37,7 @@ env.observation_space = gym.spaces.Box(low=0, high=255, shape=(4, 84, 84), dtype
 ##model = PPO.load("ppo_pong_final")
 #model = PPO.load(f"ppo_platformer_checkpoint_{3}")
 #model = PPO("CnnPolicy", env)
-model = DQN.load(f"snapshots/dqn_platformer_checkpoint_475000_steps", buffer_size =0)
+model = DQN.load(f"snapshots/dqn_platformer_checkpoint_200000_steps", buffer_size =0)
 
 # Reset the environment (gymnasium returns an observation and an info dict)
 obs, _ = env.reset()
